47f68abc64b3cf3b320530f1e6814562bea76ac5
zhos <1210965963@qq.com>
2021-01-01 18:55:07 +0800

    增加记录请求时间

--- src/http/HttpClient.php
+++ src/http/HttpClient.php
@@ -4,9 +4,12 @@
 namespace zhos\http;
 
 
+use http\Exception\RuntimeException;
+use zhos\Zos;
+
 class HttpClient
 {
-    public static function post(string $url, array $post = [], array $options = [])
+    public static function post(string $url, array $post = [], array $options = []) : string
     {
         $defaults = array(
             CURLOPT_POST => 1,
@@ -19,16 +22,22 @@ class HttpClient
             CURLOPT_POSTFIELDS => http_build_query($post)
         );
 
+        $starttime = time();
         $ch = curl_init();
         curl_setopt_array($ch, ($options + $defaults));
         if (!$result = curl_exec($ch)) {
-            trigger_error(curl_error($ch));
+            throw new RuntimeException(curl_error($ch));
         }
         curl_close($ch);
+        $requestTime = time() - $starttime;
+        if ($requestTime > 1) {
+            Zos::getLog()->info($url . ' ' . json_encode($post) . ' ' . $requestTime . 's');
+        }
+
         return $result;
     }
 
-    public static function get(string $url, array $get = [], array $options = [])
+    public static function get(string $url, array $get = [], array $options = []) : string
     {
         $defaults = array(
             CURLOPT_URL => $url . (strpos($url, '?') === FALSE ? '?' : '') . http_build_query($get),
@@ -37,12 +46,19 @@ class HttpClient
             CURLOPT_TIMEOUT => 5
         );
 
+        $starttime = time();
         $ch = curl_init();
         curl_setopt_array($ch, ($options + $defaults));
         if (!$result = curl_exec($ch)) {
-            trigger_error(curl_error($ch));
+            throw new RuntimeException(curl_error($ch));
         }
         curl_close($ch);
+
+        $requestTime = time() - $starttime;
+        if ($requestTime > 1) {
+            Zos::getLog()->info($url . ' ' . json_encode($get) . ' ' . $requestTime . 's');
+        }
+
         return $result;
     }
 }
